8ffff536467cde16bb7919a2e3671ffa962aa6c9
fsociety00d4t <58801059+fsociety00d4t@users.nonreply.github.com>
2023-05-19 22:27:40 +0300

    add first grid

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-console.log("🔥");
-let test2 = document.querySelector('.test');
-test2.innerHTML = "🔥";
+let mainGrid = document.getElementById('grid');
+
+for (let i=0;i<256;i++) {
+    let divs = document.createElement('div');
+    divs.classList.add('grid-elem')
+    mainGrid.appendChild(divs);
+} 
+
+
+/*
+let cell = document.createElement("div");
+cell.innerHTML = "TEXT";
+mainGrid.appendChild(cell);
+*/
